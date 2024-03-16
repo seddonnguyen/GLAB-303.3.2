c5ed34c9ecd5d20be318124ef57a3702e99d0c7a
seddonnguyen <tri.seddon.nguyen@gmail.com>
2024-03-15 21:59:35 -0600

    Reformat Code

--- src/ReadingStringFromConsole.java
+++ src/ReadingStringFromConsole.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class ReadingStringFromConsole {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
--- src/ReadingStringFromConsole2.java
+++ src/ReadingStringFromConsole2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
+
 public class ReadingStringFromConsole2 {
     public static void main(String[] args) {
-
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a character: ");
         String s = input.nextLine();
